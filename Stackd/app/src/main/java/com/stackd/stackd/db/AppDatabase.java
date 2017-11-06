b4afb16b0f87c1b042a69375526f4006d1a327e6
Musa Talluzi <musa.talluzi@gmail.com>
2017-11-06 11:36:40 -0500

    Added entities and daos to AppDatabase

--- Stackd/app/src/main/java/com/stackd/stackd/db/AppDatabase.java
+++ Stackd/app/src/main/java/com/stackd/stackd/db/AppDatabase.java
@@ -3,7 +3,29 @@ package com.stackd.stackd.db;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.RoomDatabase;
 
-@Database(version = 1, entities = {})
+import com.stackd.stackd.db.daos.CompanyDao;
+import com.stackd.stackd.db.daos.CompanyTagDao;
+import com.stackd.stackd.db.daos.RecruiterDao;
+import com.stackd.stackd.db.daos.ResumeDao;
+import com.stackd.stackd.db.daos.ResumeTagDao;
+import com.stackd.stackd.db.daos.ReviewDao;
+import com.stackd.stackd.db.daos.TagDao;
+import com.stackd.stackd.db.entities.Company;
+import com.stackd.stackd.db.entities.CompanyTag;
+import com.stackd.stackd.db.entities.Recruiter;
+import com.stackd.stackd.db.entities.Resume;
+import com.stackd.stackd.db.entities.ResumeTag;
+import com.stackd.stackd.db.entities.Review;
+import com.stackd.stackd.db.entities.Tag;
+
+@Database(version = 1, entities = {Company.class, CompanyTag.class, Recruiter.class,
+                                    Resume.class, ResumeTag.class, Review.class, Tag.class})
 public abstract class AppDatabase extends RoomDatabase {
-    // DAOs go here ...
+    public abstract CompanyDao companyDao();
+    public abstract CompanyTagDao companyTagDao();
+    public abstract RecruiterDao recruiterDao();
+    public abstract ResumeDao resumeDao();
+    public abstract ResumeTagDao resumeTagDao();
+    public abstract ReviewDao reviewDao();
+    public abstract TagDao tagDao();
 }
