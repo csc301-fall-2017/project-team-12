28df45ed27e06d1430272a3688d485879fcef9e0
Musa Talluzi <musa.talluzi@gmail.com>
2017-11-06 11:46:28 -0500

    Add an exampl for MVVM architecture

---
+++ Stackd/app/src/main/java/com/stackd/stackd/model/Resume.java
@@ -0,0 +1,17 @@
+package com.stackd.stackd.model;
+
+import com.stackd.stackd.db.entities.Tag;
+
+import java.util.List;
+
+/**
+ * Created by Musa on 11/6/2017.
+ */
+
+public interface Resume {
+    List<Resume> gitAllResumes();
+    List<Resume> gitResumesByTag(Tag tag);
+    List<Resume> getResume(String recruiterOrDate);
+    void insertResume(Resume resume);
+    void addReview(int recId, int resId, String date, String comment, int rating);
+}
