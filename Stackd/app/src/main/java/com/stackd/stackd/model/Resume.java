31c13e408f0fc1f8ee9d3844af1099b271438cff
Musa Talluzi <musa.talluzi@gmail.com>
2017-11-06 13:23:07 -0500

    Add model interfaces

--- Stackd/app/src/main/java/com/stackd/stackd/model/Resume.java
+++ Stackd/app/src/main/java/com/stackd/stackd/model/Resume.java
@@ -12,6 +12,7 @@ public interface Resume {
     List<Resume> gitAllResumes();
     List<Resume> gitResumesByTag(Tag tag);
     List<Resume> getResume(String recruiterOrDate);
+    boolean hasTag(Tag tag);
     void insertResume(Resume resume);
     void addReview(int recId, int resId, String date, String comment, int rating);
 }
---
+++ Stackd/app/src/main/java/com/stackd/stackd/model/Tag.java
@@ -0,0 +1,12 @@
+package com.stackd.stackd.model;
+
+/**
+ * Created by Musa on 11/6/2017.
+ */
+
+public interface Tag {
+    Long getId();
+    String getName();
+    void setName();
+
+}
