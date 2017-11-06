f964c7f23a36f529572b9ab1202d7a7a251f8ae1
Musa Talluzi <musa.talluzi@gmail.com>
2017-11-06 14:50:03 -0500

    Modify Recruiter and Resume interfaces

--- Stackd/app/src/main/java/com/stackd/stackd/model/Recruiter.java
+++ Stackd/app/src/main/java/com/stackd/stackd/model/Recruiter.java
@@ -1,5 +1,8 @@
 package com.stackd.stackd.model;
 
+import java.io.File;
+import java.util.List;
+
 /**
  * Created by Musa on 11/6/2017.
  */
@@ -7,9 +10,16 @@ package com.stackd.stackd.model;
 public interface Recruiter {
     Long getId();
     Long getCid();
-    void setCid();
     String getFirstName();
-    void setFirstName(String name);
+    void setFirstName(String firstName);
     String getLastName();
-    void setLastName();
+    void setLastName(String lastName);
+    List<Resume> getAllResumes();
+    List<Resume> getResumesByTag(Tag tag);
+    List<Resume> getResume(String recruiterOrDate);
+    List<Tag> getCompanyTags();
+    void insertResume(File resume, String data);
+    void addRating(int rating); // 0: No, 1: Maybe, 2: Yes
+    void addComment(String comment);
+
 }
--- Stackd/app/src/main/java/com/stackd/stackd/model/Resume.java
+++ Stackd/app/src/main/java/com/stackd/stackd/model/Resume.java
@@ -1,7 +1,5 @@
 package com.stackd.stackd.model;
 
-import com.stackd.stackd.db.entities.Tag;
-
 import java.util.List;
 
 /**
@@ -9,10 +7,8 @@ import java.util.List;
  */
 
 public interface Resume {
-    List<Resume> gitAllResumes();
-    List<Resume> gitResumesByTag(Tag tag);
-    List<Resume> getResume(String recruiterOrDate);
     boolean hasTag(Tag tag);
-    void insertResume(Resume resume);
     void addReview(int recId, int resId, String date, String comment, int rating);
+    void addTag(Tag tag);
+    List<Tag> getResumeTags();
 }
