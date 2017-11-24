062c4e3450c6e5f798acce060d1666e71f0e24d6
Angelo Austria <angelogabriel.austria@gmail.com>
2017-11-24 11:38:15 -0500

    Changed highlight color from green to blue for testing purposes

--- Stackd/app/src/main/java/com/stackd/stackd/views/DrawView.java
+++ Stackd/app/src/main/java/com/stackd/stackd/views/DrawView.java
@@ -20,7 +20,7 @@ import android.widget.ImageView;
 // Source: https://stackoverflow.com/questions/36812567/android-draw-text-on-imageview-with-finger-touch
 public class DrawView extends android.support.v7.widget.AppCompatImageView {
 
-    private int color = Color.GREEN;
+    private int color = Color.BLUE;
     private float width = 20f;
     private List<Holder> holderList = new ArrayList<Holder>();
 
